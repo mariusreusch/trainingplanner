b57e64194440eb42601421e4476d1726f8df550d
Marius Reusch <marius.reusch@gmail.com>
2017-12-09 18:31:10 +0100

    initial commit

---
+++ src/main/java/ch/mare/trainingplanner/TrainingPlannerApplication.java
@@ -0,0 +1,12 @@
+package ch.mare.trainingplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingPlannerApplication.class, args);
+	}
+}
