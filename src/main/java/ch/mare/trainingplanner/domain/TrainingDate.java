394ef9a8bd7423617356ea5b39a5f8197f3b4a24
Marius Reusch <marius.reusch@gmail.com>
2017-12-12 23:25:25 +0100

    introduce TrainingDate

--- src/main/java/ch/mare/trainingplanner/domain/TrainingDate.java
+++ src/main/java/ch/mare/trainingplanner/domain/TrainingDate.java
@@ -2,11 +2,42 @@ package ch.mare.trainingplanner.domain;
 
 import java.time.ZonedDateTime;
 
+import static java.util.Objects.requireNonNull;
+
 public class TrainingDate {
+
     private ZonedDateTime start;
     private ZonedDateTime end;
 
-    private TrainingDate() {
+    private TrainingDate(ZonedDateTime start, ZonedDateTime end) {
+        this.start = requireNonNull(start);
+        this.end = requireNonNull(end);
+        if(end.isBefore(start)){
+            throw new IllegalArgumentException();
+        }
     }
 
+    public static TrainingDateBuilder from(ZonedDateTime start) {
+        return new TrainingDateBuilder(requireNonNull(start));
+    }
+
+    public ZonedDateTime getStart() {
+        return start;
+    }
+
+    public ZonedDateTime getEnd() {
+        return end;
+    }
+
+    public static class TrainingDateBuilder {
+        private final ZonedDateTime start;
+
+        private TrainingDateBuilder(ZonedDateTime start) {
+            this.start = start;
+        }
+
+        public TrainingDate until(ZonedDateTime end) {
+            return new TrainingDate(start, end);
+        }
+    }
 }
